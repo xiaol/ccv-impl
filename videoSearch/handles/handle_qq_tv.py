491c99458495a151086613f1eb31206036721abe
klb3713 <klbgyx7@gmail.com>
2013-07-25 17:33:42 +0800

    修正 qq_tv 更新错误

--- videoSearch/handles/handle_qq_tv.py
+++ videoSearch/handles/handle_qq_tv.py
@@ -4,45 +4,78 @@ __author__ = 'klb3713'
 import sys,os
 sys.path += [os.path.dirname(os.path.dirname(os.path.abspath(__file__)))]
 from lxml import etree
-import re,pprint
+import re, pprint, json
 from common.common import getCurTime
 from common.Domain import Resource,Channel
 from common.HttpUtil import get_html
 from setting import clct_channel
 
 p_vid = re.compile('/([^/]+)\.html')
-p_number = re.compile(u'第(\d+)集')
-p_isover = re.compile(u'剧集：全(\d+)集')
-xpaths = ['//*[@id="mod_video_content"]/div/ul/li/p']
+p_number = re.compile(u'(\d+)集')
+p_isover = re.compile(u'剧集：全\d+集')
+p_update = re.compile(u'更新至\d+集')
+p_data_url = re.compile('src=[\'"](http://v.qq.com/c/[^/]+\.js).+Math.random()')
 
-def handle(url,channelId,tvNumber):
-    html = get_html(url)
-    tree = etree.HTML(html)
-    videos = []
-    for xpath_str in xpaths:
-        videos = tree.xpath(xpath_str)
-        if len(videos) > 0:
-            break
 
+'''
+    qq tv有两种页面，格式、编码都不一样，这里分开处理
+'''
+def handle(url, channelId, tvNumber):
     ret = []
     base_url = "http://v.qq.com"
-    for video in videos:
-        title = video.xpath('./a/@title')[0]
-        number = int(p_number.search(title).groups()[0])
-        if number <= tvNumber:
-            continue
-        url = base_url + video.xpath('./a/@href')[0]
-        subtitle = video.xpath('./span/text()')
-        if subtitle:
-            title += " " + subtitle[0]
-        videoId = p_vid.search(url).groups()[0]
 
-        item = buildResource(url, title, number, channelId, videoId)
-        ret.append(item)
+    if url.find("/zt/detail/") != -1:
+        html = get_html(url, "gbk")
+        try:
+            data_url = p_data_url.search(html).groups()[0]
+            data = get_html(data_url)
+            begin = data.find('{')
+            end = data.rfind('}')
+            json_str = data[begin:end+1]
+            data = json.loads(json_str)
+            video_list = data['vidlist']
+        except:
+            return []
 
-    '''检测完结'''
-    if p_isover.search(html):
-        ret.append("over")
+        for index, video in enumerate(video_list):
+            url = base_url+ video["playUrl"]
+            title = video["title"]
+            videoId = video["vid"]
+            number = index+1
+            if title.find(u"预告") != -1:
+                continue
+
+            item = buildResource(url, title, number, channelId, videoId)
+            ret.append(item)
+
+        '''检测完结'''
+        if not p_update.search(html):
+            ret.append("over")
+
+    else:
+        html = get_html(url)
+        tree = etree.HTML(html)
+        videos = tree.xpath('//*[@id="mod_video_content"]/div/ul/li/p')
+
+        for video in videos:
+            title = video.xpath('./a/@title')[0]
+            if title.find(u"预告") != -1:
+                continue
+            number = int(p_number.search(title).groups()[0])
+            if number <= tvNumber:
+                continue
+            url = base_url + video.xpath('./a/@href')[0]
+            subtitle = video.xpath('./span/text()')
+            if subtitle:
+                title += " " + subtitle[0]
+            videoId = p_vid.search(url).groups()[0]
+
+            item = buildResource(url, title, number, channelId, videoId)
+            ret.append(item)
+
+        '''检测完结'''
+        if p_isover.search(html):
+            ret.append("over")
 
     return ret
 
@@ -61,6 +94,8 @@ def buildResource(url, title, number, channelId, videoId):
 
 
 if __name__ == '__main__':
-    pprint.pprint(handle('http://v.qq.com/detail/x/xgnnne5is86cqh2.html',1,0))
-    pprint.pprint(handle('http://v.qq.com/detail/t/t7748cv5nzh74l6.html',1,0))
-
+    # pprint.pprint(handle('http://v.qq.com/detail/x/xgnnne5is86cqh2.html',1,0))
+    # pprint.pprint(handle('http://v.qq.com/detail/t/t7748cv5nzh74l6.html',1,0))
+    # pprint.pprint(handle('http://v.qq.com/detail/n/nndopnljjrwnwck.html',1,0))
+    pprint.pprint(handle('http://v.qq.com/zt/detail/hml/index.html',1,0))
+    pprint.pprint(handle('http://v.qq.com/zt/detail/index.html',1,0))
