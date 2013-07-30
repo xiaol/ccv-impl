08845a10450839bb8027dd020ce28daa45d71502
丁亚光 <dingyaguang117@gmail.com>
2013-07-30 12:24:03 +0800

    shareView

--- videoCMS/shareViews/channel.py
+++ videoCMS/shareViews/channel.py
@@ -10,6 +10,9 @@ def index(request):
     id = request.GET.get('id')
     channel = clct_channel.find_one({'_id':ObjectId(id) })
     resource = clct_resource.find_one({'channelId':str(channel['_id'])})
+
+    channel['detailLeadingRole'] = '/'.join(channel['detailLeadingRole'])
+    channel['detailMovieCategory'] = '/'.join(channel['detailMovieCategory'])
     DICT = channel
     if resource:
         DICT['videoType'] = resource['videoType']
