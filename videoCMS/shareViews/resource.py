919c60bf23373b4ee3d94f2f9f6106703182154c
丁亚光 <dingyaguang117@gmail.com>
2013-08-29 22:33:30 +0800

    更新下载地址

--- videoCMS/shareViews/resource.py
+++ videoCMS/shareViews/resource.py
@@ -37,7 +37,7 @@ def index(request):
         if 'videoUrl' not in DICT:
             DICT['videoUrl'] = resource['resourceUrl']
 
-    DICT['apkUrl'] = 'http://www.weiweimeishi.com/static/file/PocketPlayer1.5.1_official_website.apk'
+    DICT['apkUrl'] = 'http://koudaiv.com/static/file/PocketPlayer1.5.4_official_website.apk'
     
     return render_to_response('share_resource.htm',DICT)
     
