3cd3a594059e7e6ba4dd1d32e8ea74bdcb02583b
丁亚光 <dingyaguang117@gmail.com>
2013-10-14 11:16:29 +0800

    更新

--- videoCMS/common/db.py
+++ videoCMS/common/db.py
@@ -1,10 +1,10 @@
 from videoCMS.conf import clct_category
 
 def getCategoryNameById(id):
-    return clct_category.find_one({'categoryId':id})['categoryName']
+    return clct_category.find_one({'categoryId':id}).get('categoryName','Not Found')
 
 def getCategoryIdByName(name):
-    return clct_category.find_one({'categoryName':name})['categoryId']
+    return clct_category.find_one({'categoryName':name}).get('categoryId','Not Found')
 
 def getCategoryList():
     return clct_category.distinct('categoryName') 
