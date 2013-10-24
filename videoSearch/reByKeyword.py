e4223d6765a5228727249170b2b197896df148ed
刘潇 <liuxiao.in@gmail.com>
2013-10-24 10:44:26 +0800

    fixed bug, retrieve history log sort error

--- videoSearch/reByKeyword.py
+++ videoSearch/reByKeyword.py
@@ -48,7 +48,7 @@ def retrieveSuggestion(sinaToken):
     return result
 
 def retrieveUserHistory(userId):
-    rets = clct_playLog.find({'uuid': userId, 'playTime': { '$ne': '0' }}).sort("operationTime", 1).limit(10)
+    rets = clct_playLog.find({'uuid': userId, 'playTime': { '$ne': "0" }}).sort("operationTime", -1).limit(10)
     records = []
     for ret in rets: records.append(ret['resourceId'])
     reSet = set(records)
