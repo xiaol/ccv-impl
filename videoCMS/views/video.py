98587c14ec694b8e32043bbeed6ab2b24ccfd3d2
丁亚光 <dingyaguang117@gmail.com>
2013-10-29 17:43:48 +0800

    预览增加老封面；增加图片有效性校验

--- videoCMS/views/video.py
+++ videoCMS/views/video.py
@@ -1,10 +1,10 @@
 #coding=utf-8
 from django.http import HttpRequest,HttpResponse,HttpResponseRedirect
 from django.shortcuts import render_to_response
-from videoCMS.conf import userList,clct_resource
+from videoCMS.conf import userList,clct_resource,IMG_INTERFACE_FF
 from videoCMS.common.HttpUtil import getVideoUrl
 from videoCMS.views.resource import saveResourceImage
-import base64
+import base64,Image
 from bson import ObjectId
 
 def play(request):
@@ -12,9 +12,12 @@ def play(request):
     videoId = request.GET.get('videoId')
     videoType = request.GET.get('videoType')
 
-    resourceId = str(clct_resource.find_one({'videoType':videoType,'videoId':videoId})['_id'])
+    resource = clct_resource.find_one({'videoType':videoType,'videoId':videoId})
+    resourceId = str(resource['_id'])
 
     if request.method == 'GET':
+        DICT['curImage'] = IMG_INTERFACE_FF%('600','*',resource['resourceImageUrl'])
+
         urls = getVideoUrl(videoType,videoId)
         if urls:
             DICT['videoUrl'] = urls[0]
@@ -24,6 +27,10 @@ def play(request):
 
     img = request.POST.get('image')[22:]
     img = base64.decodestring(img)
+    try:
+        Image.open(img)
+    except:
+        return HttpResponse('图片不正确！')
     file = saveResourceImage(img,resourceId)
     clct_resource.update({'_id':ObjectId(resourceId)},{'$set':{'resourceImageUrl':file}})
 
