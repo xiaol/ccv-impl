524deb6994fa82938c5bf3080c84dbc89574f49e
丁亚光 <dingyaguang117@gmail.com>
2013-12-11 16:02:04 +0800

    增加视频存在监测

--- videoCMS/views/resource.py
+++ videoCMS/views/resource.py
@@ -301,6 +301,8 @@ def getVideoId(request):
     resp = json.loads(urllib2.urlopen('http://60.28.29.38:9090/api/getVideoId',json.dumps(query)).read())
     ret['videoType'] = resp['videoType']
     ret['videoId'] = resp['videoId']
+    if clct_resource.find_one(ret):
+        ret['exists'] = True
     return HttpResponse(json.dumps(ret))
 
 
