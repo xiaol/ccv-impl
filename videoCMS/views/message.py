967a0f87409a1ddbd74cce9c269c3c10619021e4
丁亚光 <dingyaguang117@gmail.com>
2014-01-16 13:25:26 +0800

    增加消息标记功能

--- videoCMS/views/message.py
+++ videoCMS/views/message.py
@@ -8,10 +8,10 @@ from bson import ObjectId
 
 def getMessageNum(request):
     username = request.session['username']
-    unreadNum = clct_cmsMessage.find({'to':{'$in':['',username]},'readed':{'$exists':False}}).count()
+    unreadNum = clct_cmsMessage.find({'to':{'$in':['',username]},'readed':{'$exists':False},'mark':{'$exists':False}}).count()
     readedNum = clct_cmsMessage.find({'to':{'$in':['',username]},'readed':True}).count()
-
-    return {'unreadMessageNum':unreadNum,'readedMessageNum':readedNum,'allMessageNum':unreadNum+readedNum}
+    markedNum = clct_cmsMessage.find({'to':{'$in':['',username]},'readed':{'$exists':False},'mark':{'$exists':True}}).count()
+    return {'unreadMessageNum':unreadNum,'readedMessageNum':readedNum,'allMessageNum':unreadNum+readedNum,'markedMessageNum':markedNum}
 
 
 
@@ -54,3 +54,26 @@ def all(request):
 def flagRead(request):
     clct_cmsMessage.update({'_id':ObjectId(request.GET.get('id'))},{'$set':{'readed':True}})
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+@NeedLogin
+def flagUnread(request):
+    clct_cmsMessage.update({'_id':ObjectId(request.GET.get('id'))},{'$unset':{'readed':True}})
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+def markMessage(request):
+    id = request.GET['id']
+    mark = request.GET['mark']
+    clct_cmsMessage.update({'_id':ObjectId(id)},{'$set':{'mark':mark}})
+    return  HttpResponse('ok')
+
+
+@NeedLogin
+def marked(request):
+    DICT = {}
+    username = request.session['username']
+    result = clct_cmsMessage.find({'to':{'$in':['',username]},'readed':{'$exists':False},'mark':{'$exists':True}}).sort([('_id',-1)])
+    result = list(result)
+    for one in result:one['id'] = str(one['_id'])
+    count = len(result)
+
+    return render_to_response('message.htm',locals(),context_instance=RequestContext(request,processors=[getMessageNum]))
