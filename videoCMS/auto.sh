93e2c091de41d19034cdfd75e77539db57502b42
丁亚光 <dingyaguang117@gmail.com>
2014-02-24 14:53:55 +0800

    fx

--- videoCMS/auto.sh
+++ videoCMS/auto.sh
@@ -3,4 +3,4 @@
 git pull origin
 #/usr/local/apache2/bin/httpd -k restart
 pkill -9 -f "/videoCMS/videoCMS/wsgi.py"
-uwsgi --http :8013 -M  --processes 4 --wsgi-file /usr/local/apache2/djangoapp/videoCMS/videoCMS/wsgi.py -d log.log --stats stats.socket
+/usr/local/python2.7/bin/uwsgi --http :8013 -M  --processes 4 --wsgi-file /usr/local/apache2/djangoapp/videoCMS/videoCMS/wsgi.py -d log.log --stats stats.socket
